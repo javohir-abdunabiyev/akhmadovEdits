f2c3964e2fd209ca4de0f161ab0d9f2889bd42e9
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2025-02-07 11:55:23 +0500

    dd

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,15 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.webp'],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         portfolio: resolve(__dirname, 'src/pages/portfolio/index.html'),
-        socialnetworks: resolve(__dirname, 'src/pages/socialnetworks/index.html'),
+        socialNetworks: resolve(__dirname, 'src/pages/socialnetworks/index.html'),
         uslugi: resolve(__dirname, 'src/pages/uslugi/index.html'),
         adaptedMenu: resolve(__dirname, 'imgs/adaptedMenu.png'),
-        imgs: '/imgs/logo.png',
-        instagram: '/imgs/instagram.webp',
-        instLogo: '/imgs/instLogo.png',
-        imgs: '/imgs/logo.png',
-        telegram: '/imgs/telegram.webp',
-        tiktok: '/imgs/tiktok.png',
-        whiteTg: '/imgs/whiteTg.png',
-        whiteTiktok: '/imgs/whiteTiktok.png',
 
       },
     },
