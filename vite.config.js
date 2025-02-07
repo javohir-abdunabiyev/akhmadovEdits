037489b6aadb72385d6f941b72c24f57cbeb8ec9
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2025-02-07 11:46:23 +0500

    aaded

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,14 @@ export default defineConfig({
         socialnetworks: resolve(__dirname, 'src/pages/socialnetworks/index.html'),
         uslugi: resolve(__dirname, 'src/pages/uslugi/index.html'),
         adaptedMenu: resolve(__dirname, 'imgs/adaptedMenu.png'),
-        
+        imgs: '/imgs/logo.png',
+        instagram: '/imgs/instagram.webp',
+        instLogo: '/imgs/instLogo.png',
+        imgs: '/imgs/logo.png',
+        telegram: '/imgs/telegram.webp',
+        tiktok: '/imgs/tiktok.png',
+        whiteTg: '/imgs/whiteTg.png',
+        whiteTiktok: '/imgs/whiteTiktok.png',
 
       },
     },
